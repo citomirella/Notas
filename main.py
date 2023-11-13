954946d045ae7925d8889a7c6f3be8e3379ad418
citomirella <150628385+citomirella@users.noreply.github.com>
2023-11-13 18:05:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print('Media: ',media)
+
+if media >= 6:
+      print('Aluno aprovado')
+else:
+      print('Aluno reprovado')
